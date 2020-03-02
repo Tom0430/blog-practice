76174ff9f1bc3c678c4aa41ced431943763afeac
tomoya takahashi <qwdvgy03@gmail.com>
2020-03-02 23:56:28 +0900

    vuejs-paginate導入

--- pages/index.vue
+++ pages/index.vue
@@ -8,6 +8,16 @@
       :id="post.sys.id"
       :date="post.sys.updatedAt"
     />
+    <paginate
+      :page-count="getPageCount"
+      :page-range="3"
+      :margin-pages="2"
+      :click-handler="clickCallback"
+      :prev-text="'＜'"
+      :next-text="'＞'"
+      :container-class="'pagination'"
+      :page-class="'page-item'">
+    </paginate>
   </section>
 </template>
 
@@ -15,11 +25,36 @@
 import Card from '~/components/card.vue'
 import { createClient } from '~/plugins/contentful.js'
 
+var posts = [];
+for(var i=1; i < 100; i++){
+  posts.push('post-'+i);
+}
+
 const client = createClient()
 export default {
   transition: 'slide-left',
+  data() {
+     posts: posts,
+     parPage = 3,
+     currentPage = 1
+  },
   components: {
     Card
+  },
+  methods: {
+    clickCallback: function (pageNum) {
+      this.currentPage = Number(pageNum);
+    }
+  },
+  computed: {
+     getPosts: function() {
+      let current = this.currentPage * this.parPage;
+      let start = current - this.parPage;
+      return this.posts.slice(start, current);
+     },
+     getPageCount: function() {
+      return Math.ceil(this.posts.length / this.parPage);
+     }
   },
   asyncData({ env, params }) {
     return client
