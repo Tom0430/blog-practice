6b4756c3bc51e530d3df06b841903a78acf5e744
tomoya takahashi <qwdvgy03@gmail.com>
2020-03-03 23:55:03 +0900

    vuetify pagination導入

--- pages/index.vue
+++ pages/index.vue
@@ -1,23 +1,18 @@
 <template>
   <section class="index">
     <card
-      v-for="(post,i ) in posts"
-      :key="i"
+      v-for="post in displayPosts"
+      :key="post.index"
       :title="post.fields.title"
       :image="post.fields.images.fields.file.url"
       :id="post.sys.id"
       :date="post.sys.updatedAt"
     />
-    <paginate
-      :page-count="getPageCount"
-      :page-range="3"
-      :margin-pages="2"
-      :click-handler="clickCallback"
-      :prev-text="'＜'"
-      :next-text="'＞'"
-      :container-class="'pagination'"
-      :page-class="'page-item'">
-    </paginate>
+    <v-pagination
+      v-model="page"
+      :length="5"
+      @input = "getNumber"
+    ></v-pagination>
   </section>
 </template>
 
@@ -25,49 +20,37 @@
 import Card from '~/components/card.vue'
 import { createClient } from '~/plugins/contentful.js'
 
-var posts = [];
-for(var i=1; i < 100; i++){
-  posts.push('post-'+i);
-}
-
 const client = createClient()
 export default {
-  transition: 'slide-left',
-  data() {
-     posts: posts,
-     parPage: 3,
-     currentPage: 1
+  data () {
+    return {
+      page: 1,
+      displayPosts: [],
+      pageSize: 3,
+    }
   },
+  transition: 'slide-left',
   components: {
     Card
   },
   methods: {
-    clickCallback: function (pageNum) {
-      this.currentPage = Number(pageNum);
+    getNumber: function(number){
+      console.log(number)
     }
-  },
-  computed: {
-     getPosts: function() {
-      let current = this.currentPage * this.parPage;
-      let start = current - this.parPage;
-      return this.posts.slice(start, current);
-     },
-     getPageCount: function() {
-      return Math.ceil(this.posts.length / this.parPage);
-     }
   },
   asyncData({ env, params }) {
     return client
       .getEntries(env.CTF_BLOG_POST_TYPE_ID)
       .then(entries => {
         return {
-          posts: entries.items
+          posts: entries.items.slice(0,2)
         }
       })
       .catch(console.error)
   },
+  mounted: function(){
+    this.displayPosts = this.posts.slice(0,this.pageSize);
+
+  }
 }
 </script>
-
-//テスト
-//さらにテスト
