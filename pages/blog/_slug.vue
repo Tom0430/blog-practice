9df4eaa8b3aa9e0d3878775fd95fc42286749a98
tomoya takahashi <qwdvgy03@gmail.com>
2020-03-06 18:52:21 +0900

    詳細ページでリロード対策できた

--- pages/blog/_slug.vue
+++ pages/blog/_slug.vue
@@ -1,50 +1,39 @@
 <template>
-  <section class="slug">
-    <!-- <div v-if="article[0].fields.images.fields.file">
-      <img width="1100" height="800" :src="article[0].fields.images.fields.file.url"/>
-    </div>
-    <h1 class="slug_title">
-      {{ article[0].fields.title }}
-    </h1>
-    <p class="slug_tag">タグ：{{ article[0].fields.tag }}</p>
-    <p class="slug_date">{{ article[0].fields.dateTime }}</p>
-    <div>
-      {{ article[0].fields.body.content[0].content[0].value }}
+  <section class="slug" v-if="post">
+    <!-- <div>
+      <img width="1100" height="800" :src="post[0].fields.images.fields.file.url"/>
     </div> -->
-    <p>
-
-      {{ article }}
-    </p>
-
+    <h1 class="slug_title">
+       {{ post[0].fields.title }}
+    </h1>
+    <p class="slug_tag">タグ：{{ post[0].fields.tag }}</p>
+    <p class="slug_date">{{ post[0].fields.dateTime }}</p>
+    <div>
+      {{ post[0].fields.body.content[0].content[0].value }}
+    </div>
   </section>
 </template>
 <script>
 import { createClient } from '~/plugins/contentful.js'
+import axios from 'axios';
 
 const client = createClient()
 export default {
   data(){
     return{
-      article: null
+      post: null
     }
   },
   created() {
     this.$axios
       .get('https://cdn.contentful.com/spaces/' + process.env.CTF_SPACE_ID + '/environments/master/entries'
-          , {params: {access_token: process.env.CTF_CDA_ACCESS_TOKEN}})
+        , {params: {access_token: process.env.CTF_CDA_ACCESS_TOKEN}})
       .then(res => {
-        console.log(res.data.items)
         const title = this.$route.params.slug
-        const items = res.data.items
-        console.log(items)
-        const article = items.filter(function(item){
-          console.log(item)
+        this.post = res.data.items.filter(function(item){
           return item.fields.title === title
         })
-        console.log(article)
-        this.article = article;
-      });
+      })
   }
-
 };
 </script>
