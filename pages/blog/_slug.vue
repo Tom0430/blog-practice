3adbda1332af6f8450a7532bbb5c260e780ca19f
tomoya takahashi <qwdvgy03@gmail.com>
2020-03-06 12:15:54 +0900

    詳細ページにデータを持ってくるとこまで、表示はできず

--- pages/blog/_slug.vue
+++ pages/blog/_slug.vue
@@ -1,17 +1,21 @@
 <template>
   <section class="slug">
-    <!-- <div v-if="article.fields.images.fields.file">
-      <img width="1100" height="800" :src="article.fields.images.fields.file.url"/>
+    <!-- <div v-if="article[0].fields.images.fields.file">
+      <img width="1100" height="800" :src="article[0].fields.images.fields.file.url"/>
     </div>
     <h1 class="slug_title">
-      {{ article.fields.title }}
+      {{ article[0].fields.title }}
     </h1>
-    <p class="slug_tag">タグ：{{ article.fields.tag }}</p>
-    <p class="slug_date">{{ article.fields.dateTime }}</p>
+    <p class="slug_tag">タグ：{{ article[0].fields.tag }}</p>
+    <p class="slug_date">{{ article[0].fields.dateTime }}</p>
     <div>
-      {{ article.fields.body.content[0].content[0].value }}
+      {{ article[0].fields.body.content[0].content[0].value }}
     </div> -->
-    <p>{{$route.params.slug}}</p>
+    <p>
+
+      {{ article }}
+    </p>
+
   </section>
 </template>
 <script>
@@ -29,10 +33,17 @@ export default {
       .get('https://cdn.contentful.com/spaces/' + process.env.CTF_SPACE_ID + '/environments/master/entries'
           , {params: {access_token: process.env.CTF_CDA_ACCESS_TOKEN}})
       .then(res => {
-        console.log(res)
-        // this.article = res.data;
+        console.log(res.data.items)
+        const title = this.$route.params.slug
+        const items = res.data.items
+        console.log(items)
+        const article = items.filter(function(item){
+          console.log(item)
+          return item.fields.title === title
+        })
+        console.log(article)
+        this.article = article;
       });
-
   }
 
 };
